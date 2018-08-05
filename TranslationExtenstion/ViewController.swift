92cad8cbb3a3e740d134c31d6ad9bb70625b6e51
mfouda214 <m.fouda@robone.net>
2018-08-05 23:20:14 +0100

    Initial Commit

---
+++ TranslationExtenstion/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TranslationExtenstion
+//
+//  Created by Mohamed Sobhi  Fouda on 8/5/18.
+//  Copyright © 2018 Mohamed Sobhi Fouda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
